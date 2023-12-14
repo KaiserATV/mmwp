0757daaba7a81910774c16eba1428eb5796a66de
KaiserATV <maxmalong@duck.com>
2023-12-14 14:28:16 +0100

    cool

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 window.addEventListener("load",(e) => {
+    // document.getElementsByClassName("kat")[0].addEventListener("click",(e)=>{
+    //     console.log("ey");
+    //     window.location.href="index.html";
+    // });
+
     const dropDowns = document.getElementsByClassName("kat");
     for(const c of dropDowns){
         console.log(c);
